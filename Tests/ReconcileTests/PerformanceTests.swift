ef04b561d5535db5edff0f2d5c106a89af67b92b
elfenlaid <cursed.elfenlaid@gmail.com>
2021-03-22 14:31:46 +0300

    add performance chart (#1)

--- Tests/ReconcileTests/PerformanceTests.swift
+++ Tests/ReconcileTests/PerformanceTests.swift
@@ -2,28 +2,32 @@
 import XCTest
 
 final class PerformanceTests: XCTestCase {
-    let count = 1_000_000
-    
-    func makeData() -> ([Int], [Int]) {
+    func testNaiveReconcilePerfomance() {
+        measure(name: "Naive", Reconcile.naive)
+    }
+
+    func testMergeReconcilePerfomance() {
+        measure(name: "Merge", Reconcile.merge)
+    }
+
+    let counts = [10, 100, 1000, 10000, 100_000, 1_000_000, 10_000_000]
+
+    func makeData(count: Int) -> ([Int], [Int]) {
         (
             Array(stride(from: 0, to: count, by: 2)),
             Array(stride(from: 1, to: count, by: 2))
         )
     }
-    
-    func testNaiveReconcilePerfomance() {
-        let (lhs, rhs) = makeData()
-        
-        measure {
-            _ = Reconcile.naive(lhs, rhs)
-        }
-    }
-    
-    func testMergeReconcilePerfomance() {
-        let (lhs, rhs) = makeData()
-        
-        measure {
-            _ = Reconcile.merge(lhs, rhs)
+
+    func measure<A>(name: String, _ block: ([Int], [Int]) -> A) {
+        for count in counts {
+            let (lhs, rhs) = makeData(count: count)
+
+            let startTime = CACurrentMediaTime()
+            _ = block(lhs, rhs)
+            let timeElapsed = CACurrentMediaTime() - startTime
+            
+            print("Time: \(name) \(count) - \(timeElapsed)")
         }
     }
 }
